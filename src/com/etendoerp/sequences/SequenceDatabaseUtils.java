d88e8abd51f2eb6ec9d97c3353a012699fc5c83f
nicolasgabrielr <nicolas.re@smfconsulting.es>
2022-08-26 16:10:45 -0300

    Issue #68: Unique result Controlled
    EPL-386

--- src/com/etendoerp/sequences/SequenceDatabaseUtils.java
+++ src/com/etendoerp/sequences/SequenceDatabaseUtils.java
@@ -15,6 +15,7 @@ import org.openbravo.erpCommon.utility.OBMessageUtils;
 import org.openbravo.model.ad.domain.DimensionsList;
 import org.openbravo.model.ad.domain.Reference;
 import org.openbravo.model.ad.domain.SequenceConfig;
+import org.openbravo.model.ad.utility.Sequence;
 import org.openbravo.model.common.enterprise.DocumentType;
 
 import java.util.*;
@@ -81,7 +82,7 @@ public class SequenceDatabaseUtils {
         List<String> orgList = structureProvider.getParentList((String) parameters.get(PROPERTY_ORGANIZATION), true);
         for (String orgId : orgList) {
             parameters.put(PROPERTY_ORGANIZATION, orgId);
-            BaseOBObject sequence = searchSequence(cls, sequenceParameterList.generateWhereClause(), parameters);
+            BaseOBObject sequence = search(cls, sequenceParameterList.generateWhereClause(), parameters);
             if (sequence != null) {
                 return sequence;
             }
@@ -89,14 +90,15 @@ public class SequenceDatabaseUtils {
         return null;
     }
 
-    public static BaseOBObject searchSequence(Class<? extends BaseOBObject> cls, String whereClause, Map<String, Object> parameters) {
+    public static BaseOBObject search(Class<? extends BaseOBObject> cls, String whereClause, Map<String, Object> parameters) {
         final OBQuery<BaseOBObject> qry = OBDal.getInstance().createQuery((Class<BaseOBObject>) cls, whereClause);
         qry.setNamedParameters(parameters);
         List<BaseOBObject> listOfSequences = qry.list();
         if (listOfSequences.size() > 1) {
             throw new OBException(getNameOfADSequencesOnConflicts(parameters));
+        } else {
+            return !listOfSequences.isEmpty() ? listOfSequences.get(0) : null;
         }
-        return qry.uniqueResult();
     }
 
     private static String getNameOfADSequencesOnConflicts(Map<String, Object> parameters) {
