060f7c7f1be5b26c63be43af4b89c19fac626102
juan baez <juabaez@gmail.com>
2022-09-15 15:39:05 -0300

    Feature EPL-405: allow pre y post hooks when process orders

--- modules_core/com.smf.jobs.defaults/src/com/smf/jobs/defaults/ProcessOrders.java
+++ modules_core/com.smf.jobs.defaults/src/com/smf/jobs/defaults/ProcessOrders.java
@@ -1,109 +1,116 @@
 package com.smf.jobs.defaults;
 
+import java.text.ParseException;
+
+import javax.enterprise.inject.Any;
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
+
 import com.smf.jobs.ActionResult;
 import com.smf.jobs.Result;
 import com.smf.jobs.Action;
+
 import org.apache.commons.lang.mutable.MutableBoolean;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
+import org.openbravo.advpaymentmngt.ProcessInvoiceHook;
+import org.openbravo.advpaymentmngt.ProcessInvoiceUtil;
+import org.openbravo.advpaymentmngt.ProcessOrderHook;
+import org.openbravo.advpaymentmngt.ProcessOrderUtil;
+import org.openbravo.advpaymentmngt.dao.AdvPaymentMngtDao;
+import org.openbravo.base.weld.WeldUtils;
+import org.openbravo.client.kernel.RequestContext;
 import org.openbravo.dal.core.OBContext;
 import org.openbravo.dal.service.OBDal;
+import org.openbravo.erpCommon.utility.OBDateUtils;
 import org.openbravo.erpCommon.utility.OBError;
 import org.openbravo.erpCommon.utility.OBMessageUtils;
 import org.openbravo.model.ad.process.ProcessInstance;
 import org.openbravo.model.ad.ui.Process;
 import org.openbravo.model.common.order.Order;
 import org.openbravo.service.db.CallProcess;
+import org.openbravo.service.db.DalConnectionProvider;
 import org.openbravo.service.db.DbUtility;
+import org.openbravo.service.json.JsonUtils;
 
 /**
  * Action for processing orders.
  * Allows for the same actions available in the UI as part of a Job.
  */
 public class ProcessOrders extends Action {
-    Logger log = LogManager.getLogger();
+  Logger log = LogManager.getLogger();
 
-    private static final String C_ORDER_POST_ID = "104";
+  @Inject
+  private WeldUtils weldUtils;
 
-    @Override
-    protected ActionResult action(JSONObject parameters, MutableBoolean isStopped) {
-        var result = new ActionResult();
-        var processMessages = new StringBuilder();
-        int errors = 0;
+  //private static final String C_ORDER_POST_ID = "104";
 
-        result.setType(Result.Type.SUCCESS);
+  @Override
+  protected ActionResult action(JSONObject parameters, MutableBoolean isStopped) {
+    var result = new ActionResult();
+    var processMessages = new StringBuilder();
+    int errors = 0;
 
-        try {
-            var input = getInputContents(getInputClass());
-            var documentAction = parameters.getString("DocAction");
+    result.setType(Result.Type.SUCCESS);
 
-            log.debug("Process Invoice Action Parameters:");
-            log.debug(parameters.toString());
+    try {
+      var input = getInputContents(getInputClass());
+      var documentAction = parameters.getString("DocAction");
 
-            for (Order order : input) {
-                var message = processOrder(order, documentAction);
-                if (message.getType().equals("Error")) {
-                    errors++;
-                }
-                if (message.getMessage().isBlank()) {
-                    processMessages.append(order.getDocumentNo()).append(": ").append(message.getTitle()).append("\n");
-                } else {
-                    processMessages.append(order.getDocumentNo()).append(": ").append(message.getMessage()).append("\n");
-                }
-            }
+      log.debug("Process Invoice Action Parameters:");
+      log.debug(parameters.toString());
 
-            if (errors == input.size()) {
-                result.setType(Result.Type.ERROR);
-            } else if (errors > 0) {
-                result.setType(Result.Type.WARNING);
-            }
-
-            if (input.size() > 1) {
-                // Show the message in a pop up when more than one invoice was selected, for better readability.
-                var jsonMessage = new JSONObject();
-                jsonMessage.put("message", processMessages.toString().replaceAll("\n","<br>"));
-                result.setResponseActionsBuilder(getResponseBuilder().addCustomResponseAction("smartclientSay", jsonMessage));
-            }
-
-            result.setMessage(processMessages.toString());
-            result.setOutput(getInput());
-        } catch (JSONException e) {
-            log.error(e.getMessage(), e);
-            result.setType(Result.Type.ERROR);
-            result.setMessage(e.getMessage());
+      for (Order order : input) {
+        var message = processOrder(order, documentAction);
+        if (message.getType().equals("Error")) {
+          errors++;
         }
+        if (message.getMessage().isBlank()) {
+          processMessages.append(order.getDocumentNo()).append(": ").append(message.getTitle()).append("\n");
+        } else {
+          processMessages.append(order.getDocumentNo()).append(": ").append(message.getMessage()).append("\n");
+        }
+      }
 
-        return result;
+      if (errors == input.size()) {
+        result.setType(Result.Type.ERROR);
+      } else if (errors > 0) {
+        result.setType(Result.Type.WARNING);
+      }
+
+      if (input.size() > 1) {
+        // Show the message in a pop up when more than one invoice was selected, for better readability.
+        var jsonMessage = new JSONObject();
+        jsonMessage.put("message", processMessages.toString().replaceAll("\n", "<br>"));
+        result.setResponseActionsBuilder(getResponseBuilder().addCustomResponseAction("smartclientSay", jsonMessage));
+      }
+
+      result.setMessage(processMessages.toString());
+      result.setOutput(getInput());
+    } catch (JSONException | ParseException e) {
+      log.error(e.getMessage(), e);
+      result.setType(Result.Type.ERROR);
+      result.setMessage(e.getMessage());
     }
 
-    private OBError processOrder(Order order, String documentAction) {
-        Process process;
-        try {
-            OBContext.setAdminMode(true);
-            process = OBDal.getInstance().get(Process.class, C_ORDER_POST_ID);
-        } finally {
-            OBContext.restorePreviousMode();
-        }
+    return result;
+  }
 
-        try {
-            order.setDocumentAction(documentAction);
-            OBDal.getInstance().flush();
+  private OBError processOrder(Order order, String docAction) throws ParseException {
+    var processor = weldUtils.getInstance(ProcessOrderUtil.class);
 
-            final ProcessInstance pinstance = CallProcess.getInstance()
-                    .call(process, order.getId(), null);
+    return processor.process(
+        order.getId(),
+        docAction,
+        RequestContext.get().getVariablesSecureApp(),
+        new DalConnectionProvider(false)
+    );
+  }
 
-            return OBMessageUtils.getProcessInstanceMessage(pinstance);
-        } catch (Exception e) {
-            OBDal.getInstance().rollbackAndClose();
-            return OBMessageUtils
-                    .translateError(DbUtility.getUnderlyingSQLException(e).getMessage());
-        }
-    }
-
-    @Override
-    protected Class<Order> getInputClass() {
-        return Order.class;
-    }
+  @Override
+  protected Class<Order> getInputClass() {
+    return Order.class;
+  }
 }
---
+++ modules_core/org.openbravo.advpaymentmngt/src/org/openbravo/advpaymentmngt/ProcessOrderUtil.java
@@ -0,0 +1,149 @@
+package org.openbravo.advpaymentmngt;
+
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.enterprise.inject.Any;
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openbravo.advpaymentmngt.dao.AdvPaymentMngtDao;
+import org.openbravo.base.secureApp.VariablesSecureApp;
+import org.openbravo.dal.core.OBContext;
+import org.openbravo.dal.service.OBDal;
+import org.openbravo.data.FieldProvider;
+import org.openbravo.database.ConnectionProvider;
+import org.openbravo.erpCommon.ad_actionButton.ActionButtonUtility;
+import org.openbravo.erpCommon.reference.PInstanceProcessData;
+import org.openbravo.erpCommon.utility.OBDateUtils;
+import org.openbravo.erpCommon.utility.OBError;
+import org.openbravo.erpCommon.utility.Utility;
+import org.openbravo.model.ad.process.ProcessInstance;
+import org.openbravo.model.ad.ui.Process;
+import org.openbravo.model.common.order.Order;
+import org.openbravo.service.db.CallProcess;
+
+/**
+ * Utility with methods related to the processing of orders of any type.
+ */
+public class ProcessOrderUtil {
+    private static final Logger log4j = LogManager.getLogger();
+    private final AdvPaymentMngtDao dao = new AdvPaymentMngtDao();
+
+    @Inject
+    @Any
+    private Instance<ProcessOrderHook> hooks;
+
+    /**
+     * Processes an order.
+     * This method uses the {@link ProcessOrderHook} type hooks to execute pre and post hooks.
+     * @param strC_Order_ID Order ID
+     * @param strdocaction Document Action
+     * @param vars {@link VariablesSecureApp} Used to obtain current language and by Payment Processes. Use {@link org.openbravo.client.kernel.RequestContext#getVariablesSecureApp()} outside of servlets.
+     * @param conn {@link ConnectionProvider} Used to connect to the database. Use 'this' when in servlets.
+     * @return an {@link OBError} with the message of the resulting operation. It can be a success.
+     */
+    public OBError process(String strC_Order_ID, String strdocaction, VariablesSecureApp vars, ConnectionProvider conn) {
+        OBError myMessage = null;
+        try {
+
+            Order order = dao.getObject(Order.class, strC_Order_ID);
+            order.setDocumentAction(strdocaction);
+            OBDal.getInstance().save(order);
+            OBDal.getInstance().flush();
+
+            OBError msg = null;
+            for (ProcessOrderHook hook : hooks) {
+                msg = hook.preProcess(order, strdocaction);
+                if (msg != null && "Error".equals(msg.getType())) {
+                    return msg;
+                }
+            }
+            // check BP currency
+            if ("CO".equals(strdocaction)) {
+                // check BP currency
+                if (order.getBusinessPartner().getCurrency() == null) {
+                    String errorMSG = Utility.messageBD(conn, "InitBPCurrencyLnk", vars.getLanguage(),
+                            false);
+                    msg = new OBError();
+                    msg.setType("Error");
+                    msg.setTitle(Utility.messageBD(conn, "Error", vars.getLanguage()));
+                    msg.setMessage(String.format(errorMSG, order.getBusinessPartner().getId(),
+                            order.getBusinessPartner().getName()));
+
+                    return msg;
+                }
+            }
+
+            OBContext.setAdminMode(true);
+            Process process = null;
+            try {
+                process = dao.getObject(Process.class, "104");
+            } finally {
+                OBContext.restorePreviousMode();
+            }
+
+
+            final ProcessInstance pinstance = CallProcess.getInstance()
+                    .call(process, strC_Order_ID, null);
+
+
+            OBContext.setAdminMode();
+            try {
+                // on error close popup and rollback
+                if (pinstance.getResult() == 0L) {
+                    OBDal.getInstance().rollbackAndClose();
+                    myMessage = Utility.translateError(conn, vars, vars.getLanguage(),
+                            pinstance.getErrorMsg().replaceFirst("@ERROR=", ""));
+                    log4j.debug(myMessage.getMessage());
+
+                    return myMessage;
+                }
+            } finally {
+                OBContext.restorePreviousMode();
+            }
+
+            for (ProcessOrderHook hook : hooks) {
+                msg = hook.postProcess(order, strdocaction);
+                if (msg != null && "Error".equals(msg.getType())) {
+                    OBDal.getInstance().rollbackAndClose();
+                    return msg;
+                }
+            }
+
+            OBDal.getInstance().commitAndClose();
+            final PInstanceProcessData[] pinstanceData = PInstanceProcessData.select(conn,
+                    pinstance.getId());
+            myMessage = Utility.getProcessInstanceMessage(conn, vars, pinstanceData);
+            log4j.debug(myMessage.getMessage());
+
+        } catch (ServletException ex) {
+            myMessage = Utility.translateError(conn, vars, vars.getLanguage(), ex.getMessage());
+            return myMessage;
+        }
+
+        return myMessage;
+    }
+
+    public static List<String> getDocumentActionList(String documentStatus, String documentAction, String isProcessing, String tableId, VariablesSecureApp vars, ConnectionProvider conn) {
+        FieldProvider[] fields = ActionButtonUtility.docAction(conn, vars, documentAction, "135",
+                documentStatus, isProcessing, tableId);
+
+        List<String> actionList = new ArrayList<>();
+
+        for (FieldProvider field : fields) {
+            actionList.add(field.getField("ID"));
+        }
+
+        return actionList;
+    }
+}
