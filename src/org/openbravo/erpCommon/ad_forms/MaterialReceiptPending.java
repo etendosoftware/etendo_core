dae3543cef18ad3d7b9cfef25763aee0598299da
Agustin <agustin.rizzotti@smfconsulting.es>
2023-07-06 10:36:52 -0300

    Issue #197: processPurchaseOrder causes idle in transactions
    EPL-594: The connection is closed in the finnaly block.

--- src/org/openbravo/erpCommon/ad_forms/MaterialReceiptPending.java
+++ src/org/openbravo/erpCommon/ad_forms/MaterialReceiptPending.java
@@ -4,15 +4,15 @@
  * Version  1.1  (the  "License"),  being   the  Mozilla   Public  License
  * Version 1.1  with a permitted attribution clause; you may not  use this
  * file except in compliance with the License. You  may  obtain  a copy of
- * the License at http://www.openbravo.com/legal/license.html 
+ * the License at http://www.openbravo.com/legal/license.html
  * Software distributed under the License  is  distributed  on  an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
  * License for the specific  language  governing  rights  and  limitations
- * under the License. 
- * The Original Code is Openbravo ERP. 
- * The Initial Developer of the Original Code is Openbravo SLU 
+ * under the License.
+ * The Original Code is Openbravo ERP.
+ * The Initial Developer of the Original Code is Openbravo SLU
  * All portions are Copyright (C) 2001-2019 Openbravo SLU
- * All Rights Reserved. 
+ * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
  */
@@ -536,6 +536,12 @@ public class MaterialReceiptPending extends HttpSecureAppServlet {
       myMessage = Utility.translateError(this, vars, vars.getLanguage(), "ProcessRunError");
     } finally {
       OBContext.restorePreviousMode();
+      try {
+        if (conn != null && !conn.isClosed()) {
+          conn.close();
+        }
+      } catch (Exception ignored) {
+      }
     }
     return myMessage;
   }
