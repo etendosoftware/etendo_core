e9c4053d235ab97480b9327b062e310e87d1c9d8
Leandro <leandro.cazorla@smfconsulting.es>
2023-08-02 08:34:40 -0300

    Issue #121: Fix for discount field being calculated incorrectly.
    EPL-512

--- src/org/openbravo/erpCommon/ad_callouts/SL_Order_Product.java
+++ src/org/openbravo/erpCommon/ad_callouts/SL_Order_Product.java
@@ -4,15 +4,15 @@
  * Version  1.1  (the  "License"),  being   the  Mozilla   Public  License
  * Version 1.1  with a permitted attribution clause; you may not  use this
  * file except in compliance with the License. You  may  obtain  a copy of
- * the License at http://www.openbravo.com/legal/license.html 
+ * the License at http://www.openbravo.com/legal/license.html
  * Software distributed under the License  is  distributed  on  an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
  * License for the specific  language  governing  rights  and  limitations
- * under the License. 
- * The Original Code is Openbravo ERP. 
- * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2001-2017 Openbravo SLU 
- * All Rights Reserved. 
+ * under the License.
+ * The Original Code is Openbravo ERP.
+ * The Initial Developer of the Original Code is Openbravo SLU
+ * All portions are Copyright (C) 2001-2017 Openbravo SLU
+ * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
  */
@@ -116,15 +116,14 @@ public class SL_Order_Product extends SimpleCallout {
     }
 
     // Discount
-    BigDecimal discount = BigDecimal.ZERO;
     BigDecimal price = isTaxIncludedPriceList ? grossPriceList : netPriceList;
-    BigDecimal priceToSubtract = isTaxIncludedPriceList ? grossBaseUnitPrice : priceStd;
-    if (price.compareTo(BigDecimal.ZERO) != 0) {
-      discount = price.subtract(priceToSubtract)
-          .multiply(new BigDecimal("100"))
-          .divide(price, 2, RoundingMode.HALF_UP);
+    BigDecimal calculatedDiscount = price.subtract(priceActual)
+        .multiply(BigDecimal.valueOf(100))
+        .divide(price, 2)
+        .setScale(0, RoundingMode.HALF_UP);
+    if (calculatedDiscount.compareTo(info.getBigDecimalParameter("inpdiscount")) != 0) {
+      info.addResult("inpdiscount", calculatedDiscount);
     }
-    info.addResult("inpdiscount", discount);
 
     // Attribute Set Instance
     if (StringUtils.isNotEmpty(strMProductID)) {
