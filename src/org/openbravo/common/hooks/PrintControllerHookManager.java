0eeddc591eadd00b6f90caee38ffd17c792845bb
RubenEtendo <ruben.danielle@smfconsulting.es>
2024-09-16 10:39:54 -0300

    Feature EPL-1519: Finish test for PrintController hooking functionality
    Improve test mocking. Changed mockito dependency in core to support static mocking, necessary to mock hooking

--- src/org/openbravo/common/hooks/PrintControllerHookManager.java
+++ src/org/openbravo/common/hooks/PrintControllerHookManager.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Any;
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
