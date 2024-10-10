f50514c360ecfa6091ac3eb2ea7f0b0afa39479b
AgustinCalderon <agustin.calderon@smfconsulting.es>
2024-10-10 12:33:28 -0300

    Feature ETP-399: Updated purchase and sales invoice completion process

--- src-test/src/org/openbravo/test/process/utils/ProcessUtilsTest.java
+++ src-test/src/org/openbravo/test/process/utils/ProcessUtilsTest.java
@@ -8,7 +8,6 @@ import java.util.List;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.openbravo.base.secureApp.VariablesSecureApp;
 import org.openbravo.client.kernel.RequestContext;
@@ -19,14 +18,13 @@ import org.openbravo.test.base.OBBaseTest;
 import org.openbravo.test.base.TestConstants;
 
 import com.smf.jobs.ActionResult;
+import com.smf.jobs.Data;
 import com.smf.jobs.Result;
-import com.smf.jobs.defaults.ProcessShipment;
+import com.smf.jobs.defaults.Utils.ProcessUtils;
 
 public class ShipmentProcessTest extends OBBaseTest {
 
   public static final String DJOBS_POST_UNPOST_MESSAGE = "DJOBS_PostUnpostMessage";
-  @InjectMocks
-  private ProcessShipment processShipment;
 
   @Override
   @Before
@@ -51,10 +49,11 @@ public class ShipmentProcessTest extends OBBaseTest {
   public void testMassiveMessageHandler_AllSuccess() {
     ActionResult result = new ActionResult();
     List<ShipmentInOut> inputs = List.of(mock(ShipmentInOut.class), mock(ShipmentInOut.class));
+    Data originalInput = mock(Data.class);
     int success = 2;
     int errors = 0;
 
-    processShipment.massiveMessageHandler(result, inputs, errors, success);
+    ProcessUtils.massiveMessageHandler(result, inputs, errors, success, originalInput);
 
     assertEquals(Result.Type.SUCCESS, result.getType());
     assertEquals(String.format(OBMessageUtils.messageBD(DJOBS_POST_UNPOST_MESSAGE), success, errors),
@@ -65,10 +64,11 @@ public class ShipmentProcessTest extends OBBaseTest {
   public void testMassiveMessageHandler_AllErrors() {
     ActionResult result = new ActionResult();
     List<ShipmentInOut> inputs = List.of(mock(ShipmentInOut.class), mock(ShipmentInOut.class));
+    Data originalInput = mock(Data.class);
     int success = 0;
     int errors = 2;
 
-    processShipment.massiveMessageHandler(result, inputs, errors, success);
+    ProcessUtils.massiveMessageHandler(result, inputs, errors, success, originalInput);
 
     assertEquals(Result.Type.ERROR, result.getType());
     assertEquals(String.format(OBMessageUtils.messageBD(DJOBS_POST_UNPOST_MESSAGE), success, errors),
@@ -79,10 +79,11 @@ public class ShipmentProcessTest extends OBBaseTest {
   public void testMassiveMessageHandler_MixedResults() {
     ActionResult result = new ActionResult();
     List<ShipmentInOut> inputs = List.of(mock(ShipmentInOut.class), mock(ShipmentInOut.class));
+    Data originalInput = mock(Data.class);
     int success = 1;
     int errors = 1;
 
-    processShipment.massiveMessageHandler(result, inputs, errors, success);
+    ProcessUtils.massiveMessageHandler(result, inputs, errors, success, originalInput);
 
     assertEquals(Result.Type.WARNING, result.getType());
     assertEquals(String.format(OBMessageUtils.messageBD(DJOBS_POST_UNPOST_MESSAGE), success, errors),
