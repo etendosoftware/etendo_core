574f912cf08eaf2821cf1a122ee581f78d3539f7
Valentin Vivaldi <valentin.vivaldi@smfconsulting.es>
2025-01-27 09:09:34 -0300

    Feature ETP-1026: Reformat BaseSecureWebServiceServlet.java file

--- modules_core/com.smf.securewebservices/src/com/smf/securewebservices/service/BaseSecureWebServiceServlet.java
+++ modules_core/com.smf.securewebservices/src/com/smf/securewebservices/service/BaseSecureWebServiceServlet.java
@@ -18,6 +18,7 @@ package com.smf.securewebservices.service;
  * Contributor(s):  ______________________________________.
  ************************************************************************
  */
+
 import java.io.IOException;
 import java.io.Writer;
 
@@ -38,15 +39,14 @@ import org.openbravo.base.session.OBPropertiesProvider;
 import org.openbravo.dal.core.OBContext;
 import org.openbravo.dal.core.SessionHandler;
 import org.openbravo.database.SessionInfo;
+import org.openbravo.service.web.InvalidContentException;
+import org.openbravo.service.web.InvalidRequestException;
+import org.openbravo.service.web.ResourceNotFoundException;
 import org.openbravo.service.web.WebService;
 import org.openbravo.service.web.WebServiceUtil;
 
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.smf.securewebservices.utils.SecureWebServicesUtils;
-
-import org.openbravo.service.web.ResourceNotFoundException;
-import org.openbravo.service.web.InvalidContentException;
-import org.openbravo.service.web.InvalidRequestException;
 
 /**
  * This servlet has two main responsibilities: 1) authenticate, 2) set the
@@ -57,192 +57,190 @@ import org.openbravo.service.web.InvalidRequestException;
  * well as url parameter based authentication.
  *
  * @author mtaal
- * @deprecated
- * 
- * TODO: This class needs to be refactored
+ * @deprecated TODO: This class needs to be refactored
  */
 @Deprecated
 public class BaseSecureWebServiceServlet extends HttpServlet {
-	private static final Logger log = LogManager.getLogger();
+  private static final Logger log = LogManager.getLogger();
 
-	public static final String LOGIN_PARAM = "l";
-	public static final String PASSWORD_PARAM = "p";
+  public static final String LOGIN_PARAM = "l";
+  public static final String PASSWORD_PARAM = "p";
 
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	private static Integer wsInactiveInterval = null;
-	private static final int DEFAULT_WS_INACTIVE_INTERVAL = 60;
+  private static Integer wsInactiveInterval = null;
+  private static final int DEFAULT_WS_INACTIVE_INTERVAL = 60;
 
-	@Override
-	protected final void service(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		final boolean sessionExists = request.getSession(false) != null;
+  @Override
+  protected final void service(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    final boolean sessionExists = request.getSession(false) != null;
 
-		AllowedCrossDomainsHandler.getInstance().setCORSHeaders(request, response);
+    AllowedCrossDomainsHandler.getInstance().setCORSHeaders(request, response);
 
-		// don't process any further requests otherwise sessions are created for OPTIONS
-		// requests, the cors headers have already been set so can return
-		if (request.getMethod().equals("OPTIONS")) {
-			return;
-		}
+    // don't process any further requests otherwise sessions are created for OPTIONS
+    // requests, the cors headers have already been set so can return
+    if (request.getMethod().equals("OPTIONS")) {
+      return;
+    }
 
-		// if a stateless webservice then set the stateless flag
-		try {
-			if (AuthenticationManager.isStatelessRequest(request)) {
-				request.setAttribute(AuthenticationManager.STATELESS_REQUEST_PARAMETER, "true");
-			} else {
-				final WebService webservice = getWebService(request);
-				if (webservice != null && AuthenticationManager.isStatelessService(webservice.getClass())) {
-					request.setAttribute(AuthenticationManager.STATELESS_REQUEST_PARAMETER, "true");
-				}
-			}
-		} catch (Throwable ignore) {
-			// ignore on purpose as subclasses may manage the resolving of webservices in a
-			// different
-			// way
-			// ignore also for backward compatibility
-		}
+    // if a stateless webservice then set the stateless flag
+    try {
+      if (AuthenticationManager.isStatelessRequest(request)) {
+        request.setAttribute(AuthenticationManager.STATELESS_REQUEST_PARAMETER, "true");
+      } else {
+        final WebService webservice = getWebService(request);
+        if (webservice != null && AuthenticationManager.isStatelessService(webservice.getClass())) {
+          request.setAttribute(AuthenticationManager.STATELESS_REQUEST_PARAMETER, "true");
+        }
+      }
+    } catch (Throwable ignore) {
+      // ignore on purpose as subclasses may manage the resolving of webservices in a
+      // different
+      // way
+      // ignore also for backward compatibility
+    }
 
-		String authStr = request.getHeader("Authorization");
-		String token = null;
-		if (authStr != null && authStr.startsWith("Bearer ")) {
-			token = authStr.substring(7);
-		}
+    String authStr = request.getHeader("Authorization");
+    String token = null;
+    if (authStr != null && authStr.startsWith("Bearer ")) {
+      token = authStr.substring(7);
+    }
 
-		try {
-			DecodedJWT decodedToken = SecureWebServicesUtils.decodeToken(token);
-			if (decodedToken != null) {
-				String userId = decodedToken.getClaim("user").asString();
-				String roleId = decodedToken.getClaim("role").asString();
-				String orgId = decodedToken.getClaim("organization").asString();
-				String warehouseId = decodedToken.getClaim("warehouse").asString();
-				String clientId = decodedToken.getClaim("client").asString();
-				if (userId == null || userId.isEmpty() || roleId == null || roleId.isEmpty() || orgId == null
-						|| orgId.isEmpty() || warehouseId == null || warehouseId.isEmpty() || clientId == null || clientId.isEmpty()) {
-					throw new OBException("SWS - Token is not valid");
-				}
-				log.debug("SWS accessed by userId " + userId);
-				OBContext.setOBContext(
-						SecureWebServicesUtils.createContext(userId, roleId, orgId, warehouseId, clientId));
-				OBContext.setOBContextInSession(request, OBContext.getOBContext());
-				SessionInfo.setUserId(userId);
-				SessionInfo.setProcessType("WS");
-				SessionInfo.setProcessId("DAL");
+    try {
+      DecodedJWT decodedToken = SecureWebServicesUtils.decodeToken(token);
+      if (decodedToken != null) {
+        String userId = decodedToken.getClaim("user").asString();
+        String roleId = decodedToken.getClaim("role").asString();
+        String orgId = decodedToken.getClaim("organization").asString();
+        String warehouseId = decodedToken.getClaim("warehouse").asString();
+        String clientId = decodedToken.getClaim("client").asString();
+        if (userId == null || userId.isEmpty() || roleId == null || roleId.isEmpty() || orgId == null
+            || orgId.isEmpty() || warehouseId == null || warehouseId.isEmpty() || clientId == null || clientId.isEmpty()) {
+          throw new OBException("SWS - Token is not valid");
+        }
+        log.debug("SWS accessed by userId " + userId);
+        OBContext.setOBContext(
+            SecureWebServicesUtils.createContext(userId, roleId, orgId, warehouseId, clientId));
+        OBContext.setOBContextInSession(request, OBContext.getOBContext());
+        SessionInfo.setUserId(userId);
+        SessionInfo.setProcessType("WS");
+        SessionInfo.setProcessId("DAL");
         try {
           OBContext.setAdminMode();
           SecureWebServicesUtils.fillSessionVariables(request);
         } finally {
           OBContext.restorePreviousMode();
         }
-				try {
-					doService(request, response);
-				} finally {
-					final boolean sessionCreated = !sessionExists && null != request.getSession(false);
-					if (sessionCreated && AuthenticationManager.isStatelessRequest(request)) {
-						log.warn("Stateless request, still a session was created " + request.getRequestURL() + " "
-								+ request.getQueryString());
-					}
+        try {
+          doService(request, response);
+        } finally {
+          final boolean sessionCreated = !sessionExists && null != request.getSession(false);
+          if (sessionCreated && AuthenticationManager.isStatelessRequest(request)) {
+            log.warn("Stateless request, still a session was created " + request.getRequestURL() + " "
+                + request.getQueryString());
+          }
 
-					HttpSession session = request.getSession(false);
-					if (session != null) {
-						// HttpSession for WS should typically expire fast
-						int maxExpireInterval = getWSInactiveInterval();
-						if (maxExpireInterval == 0) {
-							session.invalidate();
-						} else {
-							session.setMaxInactiveInterval(maxExpireInterval);
-						}
-					}
-				}
-			} else {
-				throw new OBException("SWS - Token is not valid");
-			}
-		} catch (Exception e) {
-			log.warn(e.getMessage());
-			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-		}
-	}
+          HttpSession session = request.getSession(false);
+          if (session != null) {
+            // HttpSession for WS should typically expire fast
+            int maxExpireInterval = getWSInactiveInterval();
+            if (maxExpireInterval == 0) {
+              session.invalidate();
+            } else {
+              session.setMaxInactiveInterval(maxExpireInterval);
+            }
+          }
+        }
+      } else {
+        throw new OBException("SWS - Token is not valid");
+      }
+    } catch (Exception e) {
+      log.warn(e.getMessage());
+      response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+  }
 
-	private int getWSInactiveInterval() {
-		if (wsInactiveInterval == null) {
-			try {
-				wsInactiveInterval = Integer.parseInt(OBPropertiesProvider.getInstance().getOpenbravoProperties()
-						.getProperty("ws.maxInactiveInterval", Integer.toString(DEFAULT_WS_INACTIVE_INTERVAL)));
-			} catch (Exception e) {
-				wsInactiveInterval = DEFAULT_WS_INACTIVE_INTERVAL;
-			}
-			log.info("Sessions for WS calls expire after " + wsInactiveInterval
-					+ " seconds. This can be configured with ws.maxInactiveInterval property.");
-		}
+  private int getWSInactiveInterval() {
+    if (wsInactiveInterval == null) {
+      try {
+        wsInactiveInterval = Integer.parseInt(OBPropertiesProvider.getInstance().getOpenbravoProperties()
+            .getProperty("ws.maxInactiveInterval", Integer.toString(DEFAULT_WS_INACTIVE_INTERVAL)));
+      } catch (Exception e) {
+        wsInactiveInterval = DEFAULT_WS_INACTIVE_INTERVAL;
+      }
+      log.info("Sessions for WS calls expire after " + wsInactiveInterval
+          + " seconds. This can be configured with ws.maxInactiveInterval property.");
+    }
 
-		return wsInactiveInterval;
-	}
+    return wsInactiveInterval;
+  }
 
-	protected WebService getWebService(HttpServletRequest request) {
-		final String segment = WebServiceUtil.getInstance().getFirstSegment(request.getPathInfo());
-		final Object o = OBProvider.getInstance().get(segment);
-		if (o instanceof WebService) {
-			return (WebService) o;
-		}
-		return null;
-	}
+  protected WebService getWebService(HttpServletRequest request) {
+    final String segment = WebServiceUtil.getInstance().getFirstSegment(request.getPathInfo());
+    final Object o = OBProvider.getInstance().get(segment);
+    if (o instanceof WebService) {
+      return (WebService) o;
+    }
+    return null;
+  }
 
-	protected void callServiceInSuper(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		super.service(request, response);
-	}
+  protected void callServiceInSuper(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    super.service(request, response);
+  }
 
-	protected void doService(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		try {
-			if (OBContext.getOBContext() != null) {
-				if (OBContext.getOBContext().isPortalRole()) {
-					// Portal users are not granted to direct web services
-					log.error("Portal user " + OBContext.getOBContext().getUser() + " with role "
-							+ OBContext.getOBContext().getRole() + " is trying to access to non granted web service "
-							+ request.getRequestURL());
-					throw new OBSecurityException("Web Services are not granted to Portal roles");
-				} else if (!OBContext.getOBContext().isWebServiceEnabled()) {
-					log.error("User " + OBContext.getOBContext().getUser() + " with role "
-							+ OBContext.getOBContext().getRole() + " is trying to access to non granted web service "
-							+ request.getRequestURL());
-					throw new OBSecurityException(
-							"Web Services are not granted to " + OBContext.getOBContext().getRole() + " role");
-				}
-			}
-			super.service(request, response);
-			response.setStatus(200);
-		} catch (final InvalidRequestException e) {
-			SessionHandler.getInstance().setDoRollback(true);
-			response.setStatus(400);
-			final Writer w = response.getWriter();
-			w.write(WebServiceUtil.getInstance().createErrorXML(e));
-			w.close();
-		} catch (final InvalidContentException e) {
-			SessionHandler.getInstance().setDoRollback(true);
-			response.setStatus(409);
-			final Writer w = response.getWriter();
-			w.write(WebServiceUtil.getInstance().createErrorXML(e));
-			w.close();
-		} catch (final ResourceNotFoundException e) {
-			SessionHandler.getInstance().setDoRollback(true);
-			response.setStatus(404);
-			final Writer w = response.getWriter();
-			w.write(WebServiceUtil.getInstance().createErrorXML(e));
-			w.close();
-		} catch (final OBSecurityException e) {
-			SessionHandler.getInstance().setDoRollback(true);
-			response.setStatus(401);
-			final Writer w = response.getWriter();
-			w.write(WebServiceUtil.getInstance().createErrorXML(e));
-			w.close();
-		} catch (final Throwable t) {
-			log.error(t.getMessage(), t);
-			SessionHandler.getInstance().setDoRollback(true);
-			response.setStatus(500);
-			final Writer w = response.getWriter();
-			w.write(WebServiceUtil.getInstance().createErrorXML(t));
-			w.close();
-		}
-	}
+  protected void doService(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    try {
+      if (OBContext.getOBContext() != null) {
+        if (OBContext.getOBContext().isPortalRole()) {
+          // Portal users are not granted to direct web services
+          log.error("Portal user " + OBContext.getOBContext().getUser() + " with role "
+              + OBContext.getOBContext().getRole() + " is trying to access to non granted web service "
+              + request.getRequestURL());
+          throw new OBSecurityException("Web Services are not granted to Portal roles");
+        } else if (!OBContext.getOBContext().isWebServiceEnabled()) {
+          log.error("User " + OBContext.getOBContext().getUser() + " with role "
+              + OBContext.getOBContext().getRole() + " is trying to access to non granted web service "
+              + request.getRequestURL());
+          throw new OBSecurityException(
+              "Web Services are not granted to " + OBContext.getOBContext().getRole() + " role");
+        }
+      }
+      super.service(request, response);
+      response.setStatus(200);
+    } catch (final InvalidRequestException e) {
+      SessionHandler.getInstance().setDoRollback(true);
+      response.setStatus(400);
+      final Writer w = response.getWriter();
+      w.write(WebServiceUtil.getInstance().createErrorXML(e));
+      w.close();
+    } catch (final InvalidContentException e) {
+      SessionHandler.getInstance().setDoRollback(true);
+      response.setStatus(409);
+      final Writer w = response.getWriter();
+      w.write(WebServiceUtil.getInstance().createErrorXML(e));
+      w.close();
+    } catch (final ResourceNotFoundException e) {
+      SessionHandler.getInstance().setDoRollback(true);
+      response.setStatus(404);
+      final Writer w = response.getWriter();
+      w.write(WebServiceUtil.getInstance().createErrorXML(e));
+      w.close();
+    } catch (final OBSecurityException e) {
+      SessionHandler.getInstance().setDoRollback(true);
+      response.setStatus(401);
+      final Writer w = response.getWriter();
+      w.write(WebServiceUtil.getInstance().createErrorXML(e));
+      w.close();
+    } catch (final Throwable t) {
+      log.error(t.getMessage(), t);
+      SessionHandler.getInstance().setDoRollback(true);
+      response.setStatus(500);
+      final Writer w = response.getWriter();
+      w.write(WebServiceUtil.getInstance().createErrorXML(t));
+      w.close();
+    }
+  }
 }
