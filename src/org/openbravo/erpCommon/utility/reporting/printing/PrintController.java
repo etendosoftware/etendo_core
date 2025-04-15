66780064bb74949c924b8dda3b1284c4580afa7e
ivanrobledofutit <ivan.robledo@smfconsulting.es>
2025-04-15 16:01:41 +0000

    Issue #669: Fix print hooks when more than one is present
    ETP-1368

--- src/org/openbravo/erpCommon/utility/reporting/printing/PrintController.java
+++ src/org/openbravo/erpCommon/utility/reporting/printing/PrintController.java
@@ -80,6 +80,7 @@ import org.openbravo.model.common.enterprise.Organization;
 import org.openbravo.xmlEngine.XmlDocument;
 
 import com.google.common.collect.HashBasedTable;
+import com.itextpdf.kernel.pdf.PdfReader;
 
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperPrint;
@@ -780,13 +781,19 @@ public class PrintController extends HttpSecureAppServlet {
             tempOutputStream, configuration);
         // Modify the concatenated report with hooks
         ByteArrayInputStream pdfInputStream = new ByteArrayInputStream(tempOutputStream.toByteArray());
-        ServletOutputStream os = response.getOutputStream();
+
+        validatePdfInputStream(pdfInputStream);
+        pdfInputStream.reset();
+
         // Call hooks
         if (hooking.booleanValue()) {
+          ByteArrayOutputStream postProcessOutputStream = new ByteArrayOutputStream();
           Report report = reports[0];
-          setPostHookParams(report.getDocumentType(), hookParams, report.getDocumentId(), pdfInputStream, os);
+          setPostHookParams(report.getDocumentType(), hookParams, report.getDocumentId(), pdfInputStream, postProcessOutputStream);
 
           hookManager.executeHooks(hookParams, hookManager.getPostProcess());
+
+          return postProcessOutputStream;
         }
       } else {
         response.setContentType("text/html");
@@ -1547,6 +1554,21 @@ public class PrintController extends HttpSecureAppServlet {
       OBContext.restorePreviousMode();
     }
     return Preferences.YES.equals(preferenceValue);
+  }
+
+  /**
+   * Validates that the given PDF input stream can be properly read.
+   *
+   * @param pdfInputStream the input stream containing PDF data
+   * @throws ServletException if the PDF is invalid
+   */
+  private void validatePdfInputStream(ByteArrayInputStream pdfInputStream) throws ServletException {
+    try {
+      PdfReader reader = new PdfReader(pdfInputStream);
+      reader.close();
+    } catch (Exception e) {
+      throw new ServletException("PDF is invalid and cannot be signed.", e);
+    }
   }
 
   @Override
