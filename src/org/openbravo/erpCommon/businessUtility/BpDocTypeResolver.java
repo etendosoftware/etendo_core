760879006f01535db53b91e0cb763e61e0a7ad9d
marcio22 <marcio.martinelli@smfconsulting.es>
2025-12-17 14:15:31 -0300

    Feature ETP-2540: Etendo credentials and test corrections

--- src/org/openbravo/erpCommon/businessUtility/BpDocTypeResolver.java
+++ src/org/openbravo/erpCommon/businessUtility/BpDocTypeResolver.java
@@ -1,3 +1,20 @@
+/*
+ *************************************************************************
+ * The contents of this file are subject to the Etendo License
+ * (the "License"), you may not use this file except in compliance with
+ * the License.
+ * You may obtain a copy of the License at
+ * https://github.com/etendosoftware/etendo_core/blob/main/legal/Etendo_license.txt
+ * Software distributed under the License is distributed on an
+ * "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing rights
+ * and limitations under the License.
+ * All portions are Copyright © 2021–2025 FUTIT SERVICES, S.L
+ * All Rights Reserved.
+ * Contributor(s): Futit Services S.L.
+ *************************************************************************
+ */
+
 package org.openbravo.erpCommon.businessUtility;
 
 import java.util.HashMap;
