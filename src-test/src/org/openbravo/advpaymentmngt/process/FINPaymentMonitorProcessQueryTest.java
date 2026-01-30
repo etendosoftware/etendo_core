cb6de7cd3498857562c07118c2e36edf6351d031
marcio22 <marcio.martinelli@smfconsulting.es>
2026-01-30 11:59:24 +0000

    Issue #825: update license
    ETP-2811

--- src-test/src/org/openbravo/advpaymentmngt/process/FINPaymentMonitorProcessQueryTest.java
+++ src-test/src/org/openbravo/advpaymentmngt/process/FINPaymentMonitorProcessQueryTest.java
@@ -5,11 +5,11 @@
  * the License.
  * You may obtain a copy of the License at
  * https://github.com/etendosoftware/etendo_core/blob/main/legal/Etendo_license.txt
- * Software distributed under the License is  distributed on an
+ * Software distributed under the License is distributed on an
  * "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
  * implied. See the License for the specific language governing rights
  * and limitations under the License.
- * All portions are Copyright © 2021–2025 FUTIT SERVICES, S.L
+ * All portions are Copyright © 2021–2026 FUTIT SERVICES, S.L
  * All Rights Reserved.
  * Contributor(s): Futit Services S.L.
  *************************************************************************
