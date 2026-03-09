a659eb6afc47692899f6644c61bad97c21b9bfc6
Francisco Roig <francisco.roig@smfconsulting.es>
2026-03-09 16:22:12 -0300

    Issue #954: Fix Etendo License header text
    ETP-3523

--- src/com/etendoerp/common/rest/MessagesTrlWebService.java
+++ src/com/etendoerp/common/rest/MessagesTrlWebService.java
@@ -1,17 +1,18 @@
 /*
  *************************************************************************
  * The contents of this file are subject to the Etendo License
- * Version 1.0 ("License"). You may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * https://etendo.software/licenses/etendo-public-license
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
- * the License for the specific language governing rights and limitations
- * under the License.
- * The Original Code is Etendo ERP.
- * All portions are Copyright (C) 2026 Etendo Software SL
+ * (the "License"), you may not use this file except in compliance with
+ * the License.
+ * You may obtain a copy of the License at
+ * https://github.com/etendosoftware/etendo_core/blob/main/legal/Etendo_license.txt
+ * Software distributed under the License is distributed on an
+ * "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing rights
+ * and limitations under the License.
+ * All portions are Copyright © 2021–2026 FUTIT SERVICES, S.L
  * All Rights Reserved.
- ************************************************************************
+ * Contributor(s): Futit Services S.L.
+ *************************************************************************
  */
 package com.etendoerp.common.rest;
 
