c2c36e0f91e1b1d88531d9136b310ffc5a1a2dc7
Román Magnoli <roman.magnoli@smfconsulting.es>
2026-06-09 10:49:05 -0300

    Issue #1047: Update license headers and docs for Heartbeat cleanup scripts
    ETP-4184

--- src-util/modulescript/src/org/openbravo/modulescript/HBFix.java
+++ src-util/modulescript/src/org/openbravo/modulescript/HBFix.java
@@ -1,20 +1,18 @@
 /*
  *************************************************************************
- * The contents of this file are subject to the Openbravo  Public  License
- * Version  1.1  (the  "License"),  being   the  Mozilla   Public  License
- * Version 1.1  with a permitted attribution clause; you may not  use this
- * file except in compliance with the License. You  may  obtain  a copy of
- * the License at http://www.openbravo.com/legal/license.html
- * Software distributed under the License  is  distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations under
+ * The contents of this file are subject to the Etendo License
+ * (the "License"), you may not use this file except in compliance with
  * the License.
- * The Original Code is Openbravo ERP.
- * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2026 Openbravo SLU
+ * You may obtain a copy of the License at
+ * https://github.com/etendosoftware/etendo_core/blob/main/legal/Etendo_license.txt
+ * Software distributed under the License is distributed on an
+ * "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing rights
+ * and limitations under the License.
+ * All portions are Copyright © 2026 FUTIT SERVICES, S.L
  * All Rights Reserved.
- * Contributor(s):  ______________________________________.
- ************************************************************************
+ * Contributor(s): Futit Services S.L.
+ *************************************************************************
  */
 package org.openbravo.modulescript;
 
